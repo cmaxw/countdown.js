da36f5b3f471ea7a0277f05aca3809b81e38ab88
woody2shoes <chuck@lostjungles.com>
2010-11-13 14:30:22 -0600

    countdown timer

---
+++ countdown.js
@@ -0,0 +1,56 @@
+function Countdown()
+{
+    this.start_time = "25:00";
+    this.end_time = "0:00";
+    this.target_id = "countdown_timer";
+    this.paused = true;
+    this.name = 'timer';
+}
+
+Countdown.prototype.update_target = function()
+{
+    minutes = this.minutes;
+    seconds = this.seconds;
+    if(seconds < 10) seconds = "0" + seconds;
+    $("#" + this.target_id).val(minutes + ":" + seconds);
+}
+
+Countdown.prototype.set_time = function()
+{
+
+}
+
+Countdown.prototype.init = function()
+{
+    this.reset();
+    setInterval(this.name + '.tick()', 1000);
+}
+
+Countdown.prototype.tick = function()
+{
+    if(!this.paused){
+        this.seconds = this.seconds - 1;
+        if(this.seconds <= 0 && this.minutes > 0){
+            this.minutes--;
+            this.seconds = 59;
+        }
+        if(this.seconds <= 0 && this.minutes <=0){
+            this.paused = true;
+        }
+    }
+    this.update_target();
+}
+
+Countdown.prototype.start = function()
+{
+    this.reset();
+    this.paused = false;
+}
+
+Countdown.prototype.reset = function(time)
+{
+    time_ary = this.start_time.split(":");
+    this.minutes = parseInt(time_ary[0]);
+    this.seconds = parseInt(time_ary[1]);
+    this.update_target();
+}
